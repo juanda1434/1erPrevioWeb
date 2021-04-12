a999c8e18a59840401f28a21c07401688ee417d7
David Sanchez <marko56_34@hotmail.com>
2021-04-12 09:59:38 -0500

    carga noticias en principal

--- js/app.js
+++ js/app.js
@@ -1,7 +1,39 @@
 'use strict'
 
+let botones = [];
 
-
+const generarNoticia=(id,titulo,descripcion,fecha,categoria)=>{
+    let noticia= `<div class="card col-12 mt-2" >                        
+    <div class="card-body">
+      <h5 class="card-title">${titulo} - ${categoria} - ${fecha}</h5>
+      <p class="card-text">
+         ${descripcion}
+      </p>
+      <a href="#" id="${categoria}${id}" class="card-link">Ver mas...</a>
+    </div>
+  </div>`;
+  botones[botones.length]={"id":id,"btn":categoria+id};
+  return noticia;
+}
+const cargarNoticias= (url)=>{
+    fetch(url)
+    .then(r=>r.json())
+    .then(noticias=>{    
+        let todas= '';
+        let i =0;        
+        noticias.forEach(noticia => {
+            
+            if(i<3){
+todas+= generarNoticia(noticia.id,noticia.titulo,noticia.descripcion,noticia.fecha,noticia.categoria);
+            }else{
+                return false;
+            }
+           
+        i++;
+        });
+        document.getElementById("noticias").innerHTML= todas;
+    });
+}
 const cargarCategoria= (categoria,url)=>{
     fetch(url)
     .then(r=>r.json())
@@ -25,5 +57,6 @@ const cargarCategoria= (categoria,url)=>{
 }
 cargarCategoria("deportes","http://demo6497253.mockable.io/categoria/deporte");
 cargarCategoria("tecnologia","http://demo6497253.mockable.io/categoria/tecnologia");
+cargarNoticias("http://demo6497253.mockable.io/noticias");
 //document.getElementById("Categorias-Noticias").innerHTML='Cargando...';
 
