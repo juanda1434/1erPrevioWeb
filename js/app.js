5bd19762ac92a81d58be5105e138dc30db8343a8
David Sanchez <marko56_34@hotmail.com>
2021-04-12 09:42:51 -0500

    Muesta en el panel derecho lista de los titulares tecnologia y deportes los carga desde el js externo

--- js/app.js
+++ js/app.js
@@ -1,2 +1,29 @@
 'use strict'
 
+
+
+const cargarCategoria= (categoria,url)=>{
+    fetch(url)
+    .then(r=>r.json())
+    .then(deportes=>{    
+        let todas= '';
+        let i =0;
+        
+        deportes.forEach(titular => {
+            if(i<3){
+                todas+=`<li class="list-group-item" id="d${titular.id}">
+                ${titular.titulo}
+            </li>`;
+            }else{
+                return false;
+            }
+           
+        i++;
+        });
+        document.getElementById(categoria).innerHTML= todas;
+    });
+}
+cargarCategoria("deportes","http://demo6497253.mockable.io/categoria/deporte");
+cargarCategoria("tecnologia","http://demo6497253.mockable.io/categoria/tecnologia");
+//document.getElementById("Categorias-Noticias").innerHTML='Cargando...';
+
